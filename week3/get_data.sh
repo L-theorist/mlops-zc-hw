ae667d3e04a740cc155829a9d80c1c4e998191ad
L-theorist <dagshub@filipp.email>
2022-06-09 15:54:01 +0200

    started hw3, downloading data for Q2 to work

---
+++ week3/get_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ../data
+curl -sS https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2021-01.parquet > ../data/fhv_tripdata_2021-06.parquet
+curl -sS https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2021-02.parquet > ../data/fhv_tripdata_2021-07.parquet
